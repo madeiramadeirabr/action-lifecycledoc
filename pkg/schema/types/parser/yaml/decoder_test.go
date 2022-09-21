4e9c75d152346923b30244b801b364b2479d22dd
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2022-09-21 19:28:12 -0300

    test(decoder): added tests for decoded type fields

--- pkg/schema/types/parser/yaml/decoder_test.go
+++ pkg/schema/types/parser/yaml/decoder_test.go
@@ -1,6 +1,8 @@
 package yaml_test
 
 import (
+	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -21,7 +23,7 @@ types:
     value: null
 
   IntType:
-    description: Um float
+    description: Um int
     type: integer
     value: 10
 
@@ -65,20 +67,85 @@ types:
 		t.Fatal(err)
 	}
 
-	typePathsToAssert := []string{
-		"#/types/BoolType",
-		"#/types/IntType",
-		"#/types/FloatType",
-		"#/types/StringType",
-		"#/types/ArrayType",
-		"#/types/ObjectType",
+	testCases := map[string]testCase{
+		"#/types/BoolType": {
+			expectedTypeKeyword: types.ScalarType,
+			expectedDescription: "Uma boolean",
+			expectedNullable:    true,
+			typeAssertions:      newScalarValueAssertion(t, nil),
+		},
+		"#/types/IntType": {
+			expectedTypeKeyword: types.ScalarType,
+			expectedDescription: "Um int",
+			expectedNullable:    false,
+			typeAssertions:      newScalarValueAssertion(t, 10),
+		},
+		"#/types/FloatType": {
+			expectedTypeKeyword: types.ScalarType,
+			expectedDescription: "Um float",
+			expectedNullable:    false,
+			typeAssertions:      newScalarValueAssertion(t, 22.12345678901234),
+		},
+		"#/types/StringType": {
+			expectedTypeKeyword: types.ScalarType,
+			expectedDescription: "Uma string",
+			expectedNullable:    false,
+			typeAssertions:      newScalarValueAssertion(t, "option10"),
+		},
+		"#/types/ArrayType": {
+			expectedTypeKeyword: types.ArrayType,
+			expectedDescription: "Um array de strings!",
+			expectedNullable:    false,
+			typeAssertions: func(t *testing.T, typeDef types.TypeDescriber) {
+				arrayType, err := assertTypeCasting[*types.Array](t, typeDef)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				assertScalarType(t, "Sou uma string", arrayType.Items())
+			},
+		},
+		"#/types/ObjectType": {
+			expectedTypeKeyword: types.ObjectType,
+			expectedDescription: "",
+			expectedNullable:    false,
+			typeAssertions: func(t *testing.T, typeDef types.TypeDescriber) {
+				objectType, err := assertTypeCasting[*types.Object](t, typeDef)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				sliceProperties := objectType.Properties()
+				properties := make(map[string]types.TypeDescriber)
+				for i := range sliceProperties {
+					properties[sliceProperties[i].Name()] = sliceProperties[i]
+				}
+
+				assertTypes(
+					t,
+					properties,
+					map[string]testCase{
+						"id": {
+							expectedTypeKeyword: types.ScalarType,
+							expectedDescription: "",
+							expectedNullable:    false,
+							typeAssertions:      newScalarValueAssertion(t, 10),
+						},
+						"name": {
+							expectedTypeKeyword: types.ScalarType,
+							expectedDescription: "O nome do object",
+							expectedNullable:    true,
+							typeAssertions:      newScalarValueAssertion(t, nil),
+						},
+					},
+				)
+			},
+		},
 	}
 
-	for _, path := range typePathsToAssert {
-		if _, exists := schemaSpy.types[path]; !exists {
-			t.Errorf("expected type added with '%s' path, no types have been added", path)
-		}
-	}
+	assertTypes(t, schemaSpy.types, testCases)
 }
 
 type schemaStoragerSpy struct {
@@ -93,5 +160,95 @@ func (s *schemaStoragerSpy) AddType(t types.TypeDescriber) error {
 func newSchameStorageSpy() *schemaStoragerSpy {
 	return &schemaStoragerSpy{
 		types: make(map[string]types.TypeDescriber),
+	}
+}
+
+func assertTypeCasting[T *types.Scalar | *types.Array | *types.Object](t *testing.T, typeDef types.TypeDescriber) (T, error) {
+	t.Helper()
+
+	result, is := typeDef.(T)
+	if !is {
+		return nil, fmt.Errorf("expected '%T' type, received '%T'", result, typeDef)
+	}
+
+	return result, nil
+}
+
+func newScalarValueAssertion(t *testing.T, expectedValue interface{}) func(*testing.T, types.TypeDescriber) {
+	t.Helper()
+
+	return func(t *testing.T, typeDef types.TypeDescriber) {
+		t.Helper()
+		assertScalarType(t, expectedValue, typeDef)
+	}
+}
+
+func assertScalarType(
+	t *testing.T,
+	expectedValue interface{},
+	typeDef types.TypeDescriber,
+) {
+	scalar, err := assertTypeCasting[*types.Scalar](t, typeDef)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if value := scalar.Value(); !reflect.DeepEqual(value, expectedValue) {
+		t.Errorf("expected '%v' value, received '%v'", expectedValue, value)
+	}
+}
+
+type testCase struct {
+	expectedTypeKeyword types.TypeKeyword
+	expectedDescription string
+	expectedNullable    bool
+	typeAssertions      func(t *testing.T, typeDef types.TypeDescriber)
+}
+
+func assertTypes(t *testing.T, types map[string]types.TypeDescriber, testCases map[string]testCase) {
+	for path, testCase := range testCases {
+		t.Run(fmt.Sprintf("asserting %s", path), func(t *testing.T) {
+			typeDef, exists := types[path]
+			if !exists {
+				t.Errorf("expected type added with '%s' path, no types have been added", path)
+			}
+
+			assertType(
+				t,
+				typeDef,
+				testCase.expectedTypeKeyword,
+				testCase.expectedDescription,
+				testCase.expectedNullable,
+				testCase.typeAssertions,
+			)
+		})
+	}
+}
+
+func assertType(
+	t *testing.T,
+	typeDef types.TypeDescriber,
+	expectedTypeKeyword types.TypeKeyword,
+	expectedDescription string,
+	expectedNullable bool,
+	typeAssertions func(t *testing.T, typeDef types.TypeDescriber),
+) {
+	if typeKeyword := typeDef.Type(); typeKeyword != expectedTypeKeyword {
+		t.Errorf("expected type '%s', received '%s'", expectedTypeKeyword, typeKeyword)
+	}
+
+	if description := typeDef.Description(); description != expectedDescription {
+		t.Errorf("expected description '%s', received '%s'", expectedDescription, description)
+	}
+
+	if nullable := typeDef.Nullable(); nullable != expectedNullable {
+		t.Errorf("expected nullable '%v', received '%v'", expectedNullable, nullable)
+	}
+
+	if typeAssertions != nil {
+		t.Run("type assertions", func(t *testing.T) {
+			typeAssertions(t, typeDef)
+		})
 	}
 }
