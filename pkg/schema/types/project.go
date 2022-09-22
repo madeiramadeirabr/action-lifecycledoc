be71561245dd9cb97837e33c621d3b3a29cf0527
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2022-09-21 22:56:04 -0300

    feat(schema): added project and confluence pages structures

---
+++ pkg/schema/types/project.go
@@ -0,0 +1,26 @@
+package types
+
+import "errors"
+
+type Project struct {
+	name       string
+	confluence Confluence
+}
+
+func (p *Project) Name() string {
+	return p.name
+}
+
+func (p *Project) Confluence() Confluence {
+	return p.confluence
+}
+
+func NewProject(name string) (*Project, error) {
+	if len(name) < 1 {
+		return nil, errors.New("project name cannot be empty")
+	}
+
+	return &Project{
+		name: name,
+	}, nil
+}
